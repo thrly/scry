389f95c744689e52e2ef2d469ef9de69520e8e29
thrly <o.thrly@gmail.com>
2025-10-05 15:15:54 +0100

    add test for random card request

---
+++ tests/test_random.py
@@ -0,0 +1,15 @@
+from scry.request import get_random_card
+
+
+def test_random_card_request():
+    random_card = get_random_card("")
+
+    # since we're pulling a random card, there's not many
+    # specifics we can assert other than object == card.
+
+    assert random_card
+    assert isinstance(random_card, list)
+    assert len(random_card) == 1
+    assert random_card[0].get("object") == "card"
+    assert isinstance(random_card[0].get("name"), str)
+    assert isinstance(random_card[0].get("id"), str)
