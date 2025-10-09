ad52a106211adc219a2e5c819de386d99e92318d
thrly <o.thrly@gmail.com>
2025-10-09 15:18:36 +0100

    Add rarity; set capitalisation on card ingest

--- src/scry/db_insert.py
+++ src/scry/db_insert.py
@@ -8,10 +8,10 @@ def insert_cards(cards: list, timestamp: datetime, connection) -> int:
         insert_query = """
             INSERT OR REPLACE INTO cards (
             id, name, type_line, oracle_text, color_identity, colors,
-            set_code, mana_cost, cmc, price, flavor_text, added_at )
+            set_code, mana_cost, cmc, price, flavor_text, rarity, added_at )
             VALUES (
             :id, :name, :type_line, :oracle_text, :color_identity, :colors,
-            :set_code, :mana_cost, :cmc, :price, :flavor_text, :added_at )
+            :set_code, :mana_cost, :cmc, :price, :flavor_text, :rarity, :added_at )
             """
         rows = [transform_card(raw_card, timestamp) for raw_card in cards]
         cursor.executemany(insert_query, rows)
@@ -27,14 +27,15 @@ def transform_card(card_data: dict, timestamp: datetime) -> dict:
     return {
         "id": card_data["id"],
         "name": card_data["name"],
-        "type_line": card_data["type_line"],
+        "type_line": card_data["type_line"].title(),
         "oracle_text": card_data.get("oracle_text", ""),
         "color_identity": json.dumps(card_data.get("color_identity", {})),
         "colors": json.dumps(card_data.get("colors", {})),
-        "set_code": card_data.get("set", ""),
+        "set_code": card_data.get("set", "").upper(),
         "mana_cost": card_data.get("mana_cost", ""),
         "cmc": card_data["cmc"],
         "price": json.dumps(card_data.get("prices", {})),
         "flavor_text": card_data.get("flavor_text", ""),
+        "rarity": card_data.get("rarity", "").title(),
         "added_at": timestamp,
     }
