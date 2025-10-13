27790bb914f2d72dd25fafbf7ad0a9e7ebd75f93
Oliver Thurley <o.thrly@gmail.com>
2025-10-13 11:49:26 +0100

    use context manager for db connections (with auto-closing)

--- src/scry/__init__.py
+++ src/scry/__init__.py
@@ -3,7 +3,6 @@ __all__ = [
     "get_random_card",
     "get_card_list",
     "insert_cards",
-    "db_connect",
     "db_stats",
     "get_total_cards",
     "clear_database",
@@ -11,7 +10,7 @@ __all__ = [
     "transform_card",
 ]
 
-from .db_setup import create_table, db_connect, clear_database
+from .db_setup import create_table, clear_database
 from .db_insert import insert_cards, transform_card
 from .request import get_random_card, get_card_list, set_codes
 from .db_queries import db_stats, get_total_cards
--- src/scry/main.py
+++ src/scry/main.py
@@ -2,30 +2,36 @@
 # by thrly
 
 
-from . import (
-    create_table,
-    db_connect,
-)
-from .cli import (
-    build_arg_parser,
-)
+from .db_setup import create_table
+from .cli import build_arg_parser
+
+from contextlib import closing
+import sqlite3
+from pathlib import Path
+from os import makedirs
+
+# set path (if required) for db: scry/data/cards.db
+path_to_root = Path(__file__).parent.parent.parent  # project root scry/
+makedirs(path_to_root / "data/", exist_ok=True)
 
 
 def main(argv=None):
-    connection = db_connect()
 
     # setup argument parsing (argv for testing)
     parser = build_arg_parser()
     args = parser.parse_args(argv)
 
-    try:
-        # setup / connect to local database
-        create_table(connection)
+    # use auto-closing context manager to create db connection, pass it to handlers
+    with closing(
+        sqlite3.connect(Path(path_to_root / "data" / "cards.db"))
+    ) as connection:
 
-        # execute commands from cli arguments (see cli.py for handling)
-        args.func(args, connection)
+        try:
+            # setup / connect to local database
+            create_table(connection)
 
-    except Exception as err:
-        print("Error in main(): ", err)
-    finally:
-        connection.close()  # finally close db connection
+            # execute commands from cli arguments (see cli.py for handling)
+            args.func(args, connection)
+
+        except Exception as err:
+            print("Error in main(): ", err)
